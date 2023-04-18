57644302ed2784b81c9a8a1490879d8d0cca99b6
Alexander <113508468+AlexMokr@users.noreply.github.com>
2023-04-18 12:51:17 +0500

    Add files via upload

---
+++ app/src/main/java/com/mter/selp/ui/fragments/VideoFragment.kt
@@ -0,0 +1,39 @@
+package com.mter.selp
+
+import android.net.Uri
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.Toast
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+
+class VideoFragment : AppCompatActivity() {
+
+    var videoView: VideoView? = null
+    var mediaController: MediaController? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_video_help_breath)
+
+        videoView = findViewById(R.id.help_breath_video) as VideoView?
+
+        if (mediaController == null){
+            mediaController = MediaController(this  )
+            mediaController!!.setAnchorView(videoView)
+        }
+        videoView!!.setMediaController(mediaController)
+        videoView!!.setVideoURI(Uri.parse("android.resource://" + packageName + "/" + R.raw.hehe))
+        videoView!!.requestFocus()
+        videoView!!.start()
+        videoView!!.setOnCompletionListener {
+            Toast.makeText(applicationContext, "Video End", Toast.LENGTH_LONG).show()
+        }
+
+        videoView!!.setOnErrorListener { mediaPlayer, i, i2 ->
+            Toast.makeText(applicationContext, "Error Occered", Toast.LENGTH_LONG).show()
+            false
+        }
+    }
+}
