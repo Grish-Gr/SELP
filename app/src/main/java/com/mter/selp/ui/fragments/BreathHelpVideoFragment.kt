fc0f1df40e65126e2a01a1fd39fee5b5df02115c
Alexander <113508468+AlexMokr@users.noreply.github.com>
2023-06-12 21:05:02 +0500

    Update BreathHelpVideoFragment.kt

--- app/src/main/java/com/mter/selp/ui/fragments/BreathHelpVideoFragment.kt
+++ app/src/main/java/com/mter/selp/ui/fragments/BreathHelpVideoFragment.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import com.mter.selp.R
 import com.mter.selp.databinding.FragmentVideoHelpBreathBinding
 
 class BreathHelpVideoFragment: BaseFragment() {
 
     private lateinit var binding: FragmentVideoHelpBreathBinding
+    private var isVideoPlaying = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,21 +30,32 @@ class BreathHelpVideoFragment: BaseFragment() {
     }
 
     private fun initAction(){
-        binding.pauseHelpVideo.setOnClickListener {
-            binding.helpBreathVideo.pause()
-        }
         binding.playHelpVideo.setOnClickListener {
-            binding.helpBreathVideo.start()
+            if (isVideoPlaying){
+                it.foreground = AppCompatResources.getDrawable(it.context, R.drawable.ic_play)
+                binding.helpBreathVideo.pause()
+                isVideoPlaying = false
+            }
+            else {
+                it.foreground = AppCompatResources.getDrawable(it.context, R.drawable.ic_pause)
+                binding.helpBreathVideo.start()
+                isVideoPlaying = true
+            }
+        }
+
+        binding.back.setOnClickListener {
+            openFragment(MainFragment())
         }
         binding.restartHelpVideo.setOnClickListener {
+            binding.playHelpVideo.foreground = AppCompatResources.getDrawable(it.context, R.drawable.ic_play)
+            isVideoPlaying = false
             binding.helpBreathVideo.stopPlayback()
             binding.helpBreathVideo.resume()
         }
     }
 
     private fun initHelpVideo(){
-        val helpVideo = Uri.parse("android.resource://" + activity?.packageName + "/" + R.raw.definition)
-        binding.helpBreathVideo.setVideoURI(helpVideo);
-        binding.helpBreathVideo.start()
+        val helpVideo = Uri.parse("android.resource://" + activity?.packageName + "/" + R.raw.breath)
+        binding.helpBreathVideo.setVideoURI(helpVideo)
     }
 }
