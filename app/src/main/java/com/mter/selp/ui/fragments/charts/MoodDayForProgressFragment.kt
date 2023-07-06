09455c4eff7b8b9ac83d49e0aa7ab2e05055f0e1
Alexander <A.S.Mokronosov@urfu.me>
2023-07-06 20:21:30 +0500

    add_progress_and_change_chart

---
+++ app/src/main/java/com/mter/selp/ui/fragments/charts/MoodDayForProgressFragment.kt
@@ -0,0 +1,11 @@
+package com.mter.selp.ui.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoodDayForProgressFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_day_mood_for_progress)
+    }
+}
