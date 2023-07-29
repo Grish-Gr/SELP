6774229bffc46d727039fe727ed80c382dc662d2
kseniaga <114469025+kseniaga26@users.noreply.github.com>
2023-07-29 21:28:58 +0500

    Изменение дизайна (статьи, тесты, медитации и тд)

---
+++ app/src/main/java/com/mter/selp/ArticlesFragment.kt
@@ -0,0 +1,49 @@
+package com.mter.selp.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mter.selp.R
+import com.mter.selp.databinding.FragmentArticlesBinding
+import com.mter.selp.databinding.FragmentInformationBinding
+
+class ArticlesFragment {
+    class InformationFragment: BaseFragment() {
+        private lateinit var binding: FragmentArticlesBinding
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View {
+            binding = FragmentArticlesBinding.inflate(inflater, container, false)
+            return binding.root
+        }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            initAction()
+            showHintDialog(
+                message = resources.getString(R.string.message_hint_note_dialog),
+                actionCancel = {
+                    enabledHintFragment(NOTE_HINT_DIALOG_ENABLED, false)
+                },
+                codeDialog = NOTE_HINT_DIALOG_ENABLED
+            )
+        }
+
+        private fun initAction(){
+            binding.openVideoInfo.setOnClickListener {
+                openFragment(VideoInfoFragment())
+            }
+
+            binding.back.setOnClickListener {
+                openFragment(MainFragment())
+            }
+
+            binding.openAddingArticles.setOnClickListener {
+                openFragment(AddingArticlesFragment())
+            }
+        }
+}
