2dda747e7ca57955db34f7de9c8baec0d47c8219
Grish-Gr <101037462+Grish-Gr@users.noreply.github.com>
2023-08-01 22:07:38 +0500

    Фикс конфликта MeditationFragment

--- app/src/main/java/com/mter/selp/ui/fragments/MeditationFragment.kt
+++ app/src/main/java/com/mter/selp/ui/fragments/MeditationFragment.kt
@@ -20,10 +20,10 @@ class MeditationFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initAction()
+        /*initAction()*/
     }
 
-    private fun initAction(){
+    /*private fun initAction(){
 
         binding.openMeditation2min.setOnClickListener {
             openFragment(MeditationTwoMinFragment())
@@ -32,6 +32,6 @@ class MeditationFragment : BaseFragment() {
             openFragment(MeditationThreeMinFragment())
         }
 
-    }
+    }*/
 
 }
